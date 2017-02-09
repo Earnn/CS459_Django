8188a43fe5cc406f58921fedf51083a08a42ae04
EARN <EARN>
2017-02-09 20:16:17 +0700

    web gallery with session

---
+++ week02/week02_project/myapp/views.py
@@ -0,0 +1,94 @@
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+from forms import PersonForm
+from models import Person, Image
+from django.shortcuts import render, redirect
+from datetime import datetime, timedelta
+from datetime import datetime, timedelta
+from django.utils import timezone
+from django.contrib.sessions.models import Session
+from django.contrib.sessions.backends.db import SessionStore
+from django.shortcuts import render
+
+
+def home(request):
+	return render(request, 'home.html', {'key': "value" })
+
+def CarGallery(request):
+	image_list = Image.objects.all()
+
+	# session_list = Session.objects.all()
+
+	# request.session['x'] = request.session.get('x',0) + 1
+	# request.session['y'] = request.session.get('y',0) + 1
+	# if request.session['x']==1:
+	# 	exp = timezone.localtime(timezone.now())  +timedelta(seconds=10)
+	# 	request.session.set_expiry( 60 )
+	# message="x: %s, y: %s"%( request.session['x'], request.session['y'] )
+	return render(request, 'car_gallery.html', {
+			'image_list': image_list,
+			
+		
+			 })
+
+	# listImg =[]
+def showImage(request,num="1"):
+	number = num
+	imageShow = Image.objects.get(id=number)
+	# print imageShow.image.url
+	listImg = request.session.get('key',[])
+	listImg.append(number)
+	request.session['key'] = listImg
+	if len(listImg)==1:
+		request.session.set_expiry( 5 )
+
+	for i in Session.objects.all():
+		print SessionStore().decode(i.session_data)
+    	# print SessionStore().decode(i.session_data)
+
+	return render(request, 'image.html', {
+		'imageShow': Image.objects.get(id=num),
+		'number': number,
+		'listImg':listImg,
+			 })
+
+
+	# success_url = '/'
+	# return render(request, 'car_gallery.html', {'key': "value" })
+# def KeepTrack(request):
+# 	request.session['x'] = request.session.get('x',0) + 1
+# 	request.session['y'] = request.session.get('y',0) + 1
+# 	if request.session['x']==1:
+# 		exp = timezone.localtime(timezone.now())  +timedelta(seconds=10)
+# 		request.session.set_expiry( 10 )
+# 	message="x: %s, y: %s"%( request.session['x'], request.session['y'] )
+# 	return render(request, 'car_gallery.html',
+# 		{
+# 			'message': message, 
+# 			'server_datetime_local': timezone.localtime( timezone.now() ).isoformat(),
+# 			'expiry_datetime_local': timezone.localtime( request.session.get_expiry_date() ).isoformat(),
+# 			'expiry_datetime_utc': request.session.get_expiry_date().isoformat(),
+# 			'session_list': Session.objects.all(),			
+# 		})
+# def resetx(request):
+# 	try:
+# 		del request.session['x']
+# 	except KeyError:
+# 		pass
+# 	return redirect('KeepTrack')
+
+class CreatePersonView(CreateView):
+	queryset = Person()
+	template_name='person.html'
+	form_class = PersonForm
+	success_url = '/'
+
+class UpdatePersonView(UpdateView):
+	queryset = Person.objects.all()
+	template_name='person.html'
+	form_class = PersonForm
+	success_url = '/'
+
+class ListPersonView(ListView):
+    model = Person
+    template_name='person_list.html'
